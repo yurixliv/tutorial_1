e70ee708d73a21c3552baa4a5223ecedbb452f8c
Yuri Oliveira <yuri.oliveiraxli@gmail.com>
2012-12-20 19:40:08 -0300

    validações

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,11 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
+  validates :title, :image_url, :description, presence: true
+  validates :title, uniqueness: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :image_url, allow_blank: true, format: { 
+  	with: %r{\.(gif|jpg|png)$}i, 
+  	message: 'deve ser uma URL para uma imagem GIF, JPG ou PNG'
+  }
+
 end
