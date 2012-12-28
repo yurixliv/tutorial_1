4b5dbef4e1a88e6c629d1cd07c36d5047caa9e1b
Yuri Oliveira <yuri.oliveiraxli@gmail.com>
2012-12-28 13:19:41 -0300

    Carrinho melhorado

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,21 @@
+#---
+# Excerpted from "Agile Web Development with Rails",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
+#---
 Depot::Application.routes.draw do
+  resources :line_items
+
+  resources :carts
+
   get "store/index"
 
   resources :products
 
+  # ...
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -54,6 +67,8 @@ Depot::Application.routes.draw do
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
   root to: 'store#index', as: 'store'
+  # ...
+
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
--- test/functional/store_controller_test.rb
+++ test/functional/store_controller_test.rb
@@ -1,4 +1,13 @@
+#---
+# Excerpted from "Agile Web Development with Rails",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
+#---
 require 'test_helper'
+
 class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
@@ -7,5 +16,6 @@ class StoreControllerTest < ActionController::TestCase
     assert_select '#main .entry', 3
     assert_select 'h3', 'Programming Ruby 1.9'
     assert_select '.price', /\$[,\d]+\.\d\d/
-    end
+  end
+
 end
